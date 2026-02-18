147553d334bc0e04e34f5237525a663a98a7a695
Haroldnikko08 <haroldnikko.salanguit.9@gmail.com>
2026-02-18 15:44:09 +0800

    modify file

--- chatbot/src/main/java/com/example/chatbot/ChatbotApplication.java
+++ chatbot/src/main/java/com/example/chatbot/ChatbotApplication.java
@@ -1,11 +1,24 @@
 package com.example.chatbot;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ChatbotApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(ChatbotApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner showMongoUri(@Value("${spring.data.mongodb.uri}") String uri) {
+		return args -> {
+			System.out.println("=================================");
+			System.out.println("MONGO URI BEING USED: " + uri);
+			System.out.println("=================================");
+		};
 	}
 }
